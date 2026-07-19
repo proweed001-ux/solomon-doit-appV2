7f3e5040b1d6e79cfc91c5f5b1171c1791802405
proweed001-ux <proweed001@gmail.com>
2026-07-19 11:30:04 +0700

    Augment PDF.js document cleanup typing

--- src/promo-new/env.d.ts
+++ src/promo-new/env.d.ts
@@ -1,6 +1,14 @@
 /// <reference types="vite/client" />
 
+import 'pdfjs-dist';
+
 declare module '*?url' {
   const url: string;
   export default url;
+}
+
+declare module 'pdfjs-dist' {
+  interface PDFDocumentProxy {
+    destroy(): Promise<void>;
+  }
 }
