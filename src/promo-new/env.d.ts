4c59f220a71ca48892e8315a533b45a1677e56fe
proweed001-ux <proweed001@gmail.com>
2026-07-19 11:44:26 +0700

    Remove ineffective PDF.js type augmentation

--- src/promo-new/env.d.ts
+++ src/promo-new/env.d.ts
@@ -1,14 +1,6 @@
 /// <reference types="vite/client" />
 
-import 'pdfjs-dist';
-
 declare module '*?url' {
   const url: string;
   export default url;
-}
-
-declare module 'pdfjs-dist' {
-  interface PDFDocumentProxy {
-    destroy(): Promise<void>;
-  }
 }
