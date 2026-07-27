12a924aed792cdccf1457c47ba719f4ecf7a849b
proweed001-ux <proweed001@gmail.com>
2026-07-27 07:10:32 +0700

    Type staging preview build flag

--- src/promo-new/env.d.ts
+++ src/promo-new/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 declare const __PROMO_BUILD_ID__: string;
+declare const __PROMO_STAGING_PREVIEW__: boolean;
 
 declare module '*?url' {
   const url: string;
