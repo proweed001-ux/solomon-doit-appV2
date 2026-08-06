be996d74c6a05d6e5626d6411a321987b0d61ac7
proweed001-ux <proweed001@gmail.com>
2026-08-06 09:03:46 +0700

    Initialize visible Telesale count

--- dist/assets/pro/app.js
+++ dist/assets/pro/app.js
@@ -3,9 +3,11 @@ import "./core.js";
 import { initFuelSecret } from "./fuel-secret.js";
 import { initResultsMode } from "./results-mode.js";
 import { initTeam } from "./team.js";
+import { initTelesaleButtonCount } from "./telesale.js";
 
 initFuelSecret();
 initResultsMode();
+initTelesaleButtonCount();
 initTeam();
 
 document.getElementById("fuelBillBtn")?.addEventListener("click", () => {
